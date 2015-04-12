1cd02a332c56bcdab2aab8546e57e1876ad010d3
Vittorio Cellucci <cellucciv@gmail.com>
2015-04-12 08:58:27 -0400

    Implemented simple spinlock

--- include/Pal/Spinlock.h
+++ include/Pal/Spinlock.h
@@ -9,5 +9,31 @@
 #ifndef Pal_Spinlock_h
 #define Pal_Spinlock_h
 
+#include <atomic>
+
+namespace Pal {
+    
+class Spinlock
+{
+public:
+    Spinlock()
+    {
+        flag.clear(std::memory_order_release);
+    }
+    
+    void lock()
+    {
+        while (flag.test_and_set(std::memory_order_acquire)) {}
+    }
+    
+    void unlock()
+    {
+        flag.clear(std::memory_order_release);
+    }
+    
+protected:
+    std::atomic_flag flag;
+};
+}
 
 #endif

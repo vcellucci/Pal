8a7f89b96a3f74e7a2eedd0609d6cfa32d5d59e0
Vittorio Cellucci <cellucciv@gmail.com>
2015-04-14 20:23:08 -0400

    Finished for_each_range

--- include/Pal/Algorithms.h
+++ include/Pal/Algorithms.h
@@ -15,34 +15,88 @@
 
 namespace Pal {
     
-template<class Iterator, class Callable>
-void parallel_for_each(Iterator begin, Iterator end, Callable callable)
+template<class Iterator>
+struct chunk_range
 {
-    TaskPool<details::spmc_queue, void( Iterator, Iterator ) > taskPool;
+    Iterator begin;
+    Iterator end;
+    
+    chunk_range(const Iterator& b, const Iterator& e)
+    :begin(b)
+    ,end(e)
+    {
+        
+    }
+};
+    
+template<>
+struct chunk_range<int>
+{
+    int begin;
+    int end;
+    
+    chunk_range(int b, int e)
+    :begin(b)
+    ,end(e)
+    {
+        
+    }
+};
+    
+template<class Iterator>
+std::size_t distance_wrapper(Iterator begin, Iterator end)
+{
+    return std::distance(begin, end);
+}
+    
+template<>
+std::size_t distance_wrapper<int>(int begin, int end)
+{
+    return (end - begin);
+}
+    
+template<class Iterator, class Callable>
+void for_each_range(Iterator begin, Iterator end, Callable callable)
+{
+    TaskPool<details::spmc_queue, void(chunk_range<Iterator>)> taskPool;
     
     std::size_t numThreads = taskPool.getNumThreads();
-    std::size_t numElements = std::distance(begin, end);
+    std::size_t numElements = distance_wrapper(begin, end);
     std::size_t chunkSize   = numElements / numThreads;
     std::size_t numChunks   = numElements / chunkSize;
     std::size_t leftOver    = numElements - (numChunks*chunkSize);
-    
     
     auto it = begin;
     for (std::size_t i = 0; i < numChunks; i++)
     {
         auto first = it;
         auto last  = it + chunkSize;
-        taskPool.submit(callable, first, last);
+        chunk_range<Iterator> chunk(first, last);
+        taskPool.submit(callable, chunk);
         it += chunkSize;
     }
     
     if(leftOver)
     {
         auto last = it + leftOver;
-        taskPool.submit(callable, it, last);
+        chunk_range<Iterator> chunk(it, last);
+        taskPool.submit(callable, chunk);
     }
 }
-
+    
+template<class Iterator, class Callable>
+void for_each(Iterator begin, Iterator end, Callable callable)
+{
+    TaskPool<details::spmc_queue, void( Iterator, Iterator ) > taskPool;
+    
+    for_each_range(begin, end, [callable](chunk_range<Iterator> range)
+    {
+        for(auto it = range.begin; it != range.end; ++it)
+        {
+            callable(std::ref(*it));
+        }
+    });
+}
     
 }
 
--- tests/AlgorithmTests.cpp
+++ tests/AlgorithmTests.cpp
@@ -12,19 +12,14 @@
 #include "Pal/AlignedAllocator.h"
 
 
-TEST_F(AlgorithmTests, testParallelForEach)
+TEST_F(AlgorithmTests, testForEach)
 {
     std::vector<int,Pal::aligned_allocator<int>> intVector(257, 1);
-    using Iterator = std::vector<int, Pal::aligned_allocator<int> >::iterator;
     
     // assign 2 to each element in the vector
-    Pal::parallel_for_each(intVector.begin(), intVector.end(), [](Iterator begin, Iterator end)
+    Pal::for_each(intVector.begin(), intVector.end(), [](int& val)
     {
-        // each task gets a chunk of work
-        for(auto it = begin; it != end; ++it )
-        {
-            *it = 2;
-        }
+        val = 2;
     });
     
     
@@ -34,18 +29,16 @@ TEST_F(AlgorithmTests, testParallelForEach)
     });
 }
 
+
 TEST_F(AlgorithmTests, testParallelForEachArray)
 {
     
     Pal::aligned_allocator<int> allocator;
     int* array  = allocator.allocate(256);
     
-    Pal::parallel_for_each(array, array + 256, [](int* begin, int* end)
+    Pal::for_each(array, array + 256, [](int& val)
     {
-       for(auto it = begin; it != end; ++it )
-       {
-           *it = 2;
-       }
+        val = 2;
     });
     
     std::for_each(array, array + 256, [](int& val)
@@ -54,6 +47,47 @@ TEST_F(AlgorithmTests, testParallelForEachArray)
     });
     
     allocator.deallocate(array);
+}
+
+TEST_F(AlgorithmTests, testBlockedRange)
+{
+    using IntVector =std::vector<int,Pal::aligned_allocator<int>>;
+    IntVector intVector(257, 1);
+    Pal::for_each_range(intVector.begin(), intVector.end(),
+    [](Pal::chunk_range<IntVector::iterator> range)
+    {
+        for(auto it = range.begin; it != range.end; ++it)
+        {
+            *it = 2;
+        }
+    });
     
+    std::for_each(intVector.begin(), intVector.end(), [](int& val)
+    {
+        ASSERT_EQ(2, val);
+    });
+}
+
+
+TEST_F(AlgorithmTests, testBlockedRangeArray)
+{
+    Pal::aligned_allocator<int> allocator;
+    int* array  = allocator.allocate(256);
+    
+    Pal::for_each_range(0, 256,
+    [&array](Pal::chunk_range<int> range)
+    {
+        for(auto it = range.begin; it != range.end; ++it)
+        {
+            array[it] = 2;
+        }
+    });
+    
+    std::for_each(array, array + 256, [](int& val)
+    {
+        ASSERT_EQ(2, val);
+    });
+    
+    allocator.deallocate(array);
 }
 
